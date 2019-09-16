46959cb70fbfb01743a02f338359c85829df0ce5
Siddharth Chandra <void-trinity@users.noreply.github.com>
2019-09-16 11:16:42 +0530

    Add files via upload

---
+++ Program14/Program15/Program15Client.java
@@ -0,0 +1,27 @@
+package Program15;
+
+import java.io.*;
+import java.net.*;
+public class Program15Client {
+    public static void main(String args[]) {
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 8888);
+            DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = "Hello World!\n";
+            toServer.writeBytes(message);
+            String receivedMessage = fromServer.readLine();
+            System.out.println("Received Message: " + receivedMessage);
+            while(true) {
+                message = fromServer.readLine();
+                if(message.trim().equals("Bye"))
+                    break;
+                System.out.println(message);
+            }
+            socket.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
